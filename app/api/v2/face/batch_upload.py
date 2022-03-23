d5ba81eea2a3f93972df84e16f5a78de0c714e15
shinoi2 <shinoi@mail.hfut.edu.cn>
2022-03-23 10:53:05 +0800

    batch_upload

--- app/api/v2/face/batch_upload.py
+++ app/api/v2/face/batch_upload.py
@@ -4,7 +4,36 @@ from . import face_bp
 
 @face_bp.route('/batch_upload', methods=['PUT', 'POST'])
 def batch_upload():
-    return jsonify({
-        "code": 2,
-        "msg": "人脸批量上传未实现"
-    }), 500
+    faceIds, images = get_image_v2()
+    if images is None:
+        return jsonify({
+            "msg": "images 参数不存在",
+            "code": 1
+        }), 400
+    i = 0
+    failed_id = ''
+    for image in images:
+        image = cv2.imencode('.jpg', image)[1].tobytes()
+        repo_id = request.form.get("repo_id")
+        if repo_id is None:
+            return jsonify({
+                "msg": "repo_id 参数不存在",
+                "code": 1,
+            }), 400
+        face_id = faceIds[i]
+        rtn = face_recognition_client.upload(image, face_id, repo_id)
+        if rtn != 0:
+            failed_id = failed_id + face_id + ','
+        i = i + 1
+    if failed_id:
+        return jsonify({
+                "code": 2,
+                "msg": "人脸批量上传未实现(" + failed_id + "上传失败)"
+            }), 500
+    def format_data():
+        return {
+            "msg": "OK",
+            "code": 0
+        }
+
+    return jsonify(format_data())
--- app/common/__init__.py
+++ app/common/__init__.py
@@ -2,6 +2,11 @@ from flask import request, current_app
 import cv2
 import numpy as np
 import urllib
+import zipfile
+from werkzeug.utils import secure_filename
+from werkzeug.datastructures import FileStorage
+import os
+import shutil
 
 def get_image_v1(image='image'):
     if request.files[image]:
@@ -11,7 +16,7 @@ def get_image_v1(image='image'):
         return img
     return None
 
-def get_image_v2(image='image', url='url'):
+def get_image_v2(image='image', url='url',images='images'):
     if request.files.get(image) is not None:
         img = cv2.imdecode(np.fromstring(request.files[image].read(), np.uint8), cv2.IMREAD_UNCHANGED)
         if len(img.shape) > 2 and img.shape[2] == 4:
@@ -24,5 +29,26 @@ def get_image_v2(image='image', url='url'):
         if len(img.shape) > 2 and img.shape[2] == 4:
             img = cv2.cvtColor(img, cv2.COLOR_BGRA2BGR)
         return img
+    if request.files.get(images) is not None:
+        profile = request.files[images]
+        img_root_dir = "./images/"
+        if not os.path.exists(img_root_dir):
+            os.mkdir(img_root_dir)
+        profile.save(os.path.join(img_root_dir, secure_filename(profile.filename)))
+        zfile = zipfile.ZipFile(os.path.join(img_root_dir, secure_filename(profile.filename)),'r')
+        faceIds = []
+        images = []
+        for name in zfile.namelist()[1:]:
+            img = cv2.imdecode(np.fromstring(zfile.read(name), np.uint8), cv2.IMREAD_UNCHANGED)
+            if len(img.shape) > 2 and img.shape[2] == 4:
+                img = cv2.cvtColor(img, cv2.COLOR_BGRA2BGR)
+            Index1 = name.rfind("/")
+            Index2 = name.find(",")
+            faceid = name[Index1+1:Index2]
+            faceIds.append(faceid)
+            images.append(img)
+        shutil.rmtree(img_root_dir)
+        return mFaceids, mImages
+    
 
     return None
