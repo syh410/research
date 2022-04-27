9151795c5c2ce04b0e1831de77177b7aaeefa6ed
syh410 <90239735+syh410@users.noreply.github.com>
2022-04-27 14:06:13 +0800

    Update light.py

--- app/api/v2/light.py
+++ app/api/v2/light.py
@@ -1,10 +1,10 @@
-from light_detector import LightDetector
+from light_detector import LightDetectionClient
 from common import get_image_v2
 from flask import jsonify
 from . import v2_bp
 
 
-light_detector = LightDetector("/root/light.pth")
+light_detector = LightDetectionClient()
 @v2_bp.route('/light', methods=['POST'])
 def light():
     image = get_image_v2()
@@ -13,7 +13,7 @@ def light():
             "msg": "image 或 url 参数不存在",
             "code": 1
         })
-    result = light_detector.detection(image)
+    result = light_detector.predict(image)
 
     def format_data(result):
         return {
