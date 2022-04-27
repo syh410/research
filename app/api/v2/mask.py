1f5d35db65e9d191ff561fb0c06953a24f7f881b
syh410 <90239735+syh410@users.noreply.github.com>
2022-04-27 14:04:35 +0800

    Update mask.py

--- app/api/v2/mask.py
+++ app/api/v2/mask.py
@@ -19,22 +19,23 @@ def mask():
     results = face_detector.predict(image=image)
     data = []
     mask_count = no_mask_count = 0
-    images = []
-    rect = []
-    for  i, result in enumerate(results):
-        rect.append(result['rect'])
-        images.append(image[int(rect[i]['top'] - 1):int(rect[i]['bottom'] + 1), int(rect[i]['left'] - 1):int(rect[i]['right'] + 1)])
-    results= mask_detector.predict(images=images)
-    for i, result in enumerate(results):
-        data.append({
-                "score": result['score'],
-                "rect": rect[i],
-                "mask": result['mask'],
-            })
-        if result['mask'] == True:
-            mask_count += 1
-        else:
-            no_mask_count += 1
+    if results:
+        images = []
+        rect = []
+        for  i, result in enumerate(results):
+            rect.append(result['rect'])
+            images.append(image[int(rect[i]['top'] - 1):int(rect[i]['bottom'] + 1), int(rect[i]['left'] - 1):int(rect[i]['right'] + 1)])
+        results= mask_detector.predict(images=images)
+        for i, result in enumerate(results):
+            data.append({
+                    "score": result['score'],
+                    "rect": rect[i],
+                    "mask": result['mask'],
+                })
+            if result['mask'] == True:
+                mask_count += 1
+            else:
+                no_mask_count += 1
     def format_data(data, mask_count, no_mask_count):
         count = len(data)
         return {
